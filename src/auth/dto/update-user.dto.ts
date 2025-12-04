2b4ce23ba04538707ee497edb48a581c86163012
Kagura0904 <8.abe.sub@gmail.com>
2025-12-04 22:51:44 +0900

    changed dto

--- src/auth/dto/update-user.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, IsOptional } from 'class-validator';
-
-export class UpdateDTO {
-	@IsString()
-	@IsOptional()
-	readonly password?: string;
-}
---
+++ src/auth/schema/user.schema.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+import fi from 'zod/v4/locales/fi.js';
+
+/**
+ * User Schemas
+ */
+export const createUserSchema = {
+    firstName: z.string().max(16),
+    lastName: z.string().max(16),
+    userId: z.string().max(16),
+    password: z.string().min(8).max(32)
+};
+
+export const signInUserSchema = {
+    userId: z.string().max(16),
+    password: z.string().min(8).max(32)
+};
+
+export const updateUserSchema = {
+    firstName: z.string().max(16).optional(),
+    lastName: z.string().max(16).optional(),
+    userId: z.string().max(16).optional(),
+    password: z.string().min(8).max(32).optional()
+};
+
+export const deleteUserSchema = {
+    password: z.string().min(8).max(32)
+};
+
+export type CreateUserSchema = z.infer<typeof createUserSchema>;
+export type SignInUserSchema = z.infer<typeof signInUserSchema>;
+export type UpdateUserSchema = z.infer<typeof updateUserSchema>;
+export type DeleteUserSchema = z.infer<typeof deleteUserSchema>;
